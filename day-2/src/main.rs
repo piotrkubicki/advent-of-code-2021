02ccaf5919d754040bc4d5bb03f0018d26bb8bb5
Piotr Kubicki <pkubicki44@gmail.com>
2021-12-10 20:30:12 +0000

    Refactor day 2 task one
    Replace array with Position struct and use Drive trait to make code more generic.

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,28 +1,39 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-const X: usize = 0;
-const Y: usize = 1;
+struct Position {
+    x_pos: u32,
+    y_pos: u32,
+}
 
-fn execute_command(command: &String, position: &mut [u32; 2]) {
+trait Driver {
+    fn move_submarine(&mut self, direction: &str, strength: u32);
+}
+
+impl Driver for Position {
+    fn move_submarine(&mut self, direction: &str, strength: u32) {
+        match direction {
+            "forward" => self.x_pos += strength,
+            "down" => self.y_pos += strength,
+            "up" => self.y_pos -= strength,
+            _ => println!("Unknown command, skipping!")
+        }
+    }
+}
+
+fn execute_command<T: Driver>(command: &String, position: &mut T) {
     let command: Vec<&str> = command.split(' ').collect();
     if command.len() == 2 {
         let direction = command[0];
         let strength = command[1].parse::<u32>().expect("Cannot parse value!");
-
-        match direction {
-            "forward" => position[X] += strength,
-            "down" => position[Y] += strength,
-            "up" => position[Y] -= strength,
-            _ => println!("Unknown command, skipping!")
-        }
+        position.move_submarine(direction, strength);
     } else {
         println!("Incorrect command format!");
     }
 }
 
-fn drive<R: BufRead>(commands: &mut R) -> [u32; 2] {
-    let mut position: [u32; 2] = [0; 2];
+fn drive<R: BufRead>(commands: &mut R) -> Position {
+    let mut position: Position = Position { x_pos: 0, y_pos: 0};
 
     for command in commands.lines() {
         let command = command.expect("Unable to read line!");
@@ -35,12 +46,8 @@ fn drive<R: BufRead>(commands: &mut R) -> [u32; 2] {
 fn task_one() {
     let file = File::open("src/input.txt").expect("File cannot be opened!");
     let mut commands = BufReader::new(file);
-    let position: [u32; 2] = drive(&mut commands);
-    let mut result = 1;
-
-    for value in position {
-        result *= value;
-    }
+    let position: Position = drive(&mut commands);
+    let result = position.x_pos * position.y_pos;
 
     println!("Total: {}", result);
 }
@@ -56,39 +63,43 @@ mod tests {
     #[test]
     fn test_execute_forward_command() {
         let command = "forward 5".to_string();
-        let mut position: [u32; 2] = [0; 2];
-        let expected: [u32; 2] = [5, 0];
+        let mut position: Position = Position{ x_pos: 0, y_pos: 0};
+        let expected: Position = Position{ x_pos: 5, y_pos: 0};
 
         execute_command(&command, &mut position);
-        assert!(position.iter().zip(expected.iter()).all(|(a,b)| a == b), "Arrays are not equal");
+        assert_eq!(position.x_pos, expected.x_pos, "x_pos are not equal");
+        assert_eq!(position.y_pos, expected.y_pos, "y_pos are not equal");
     }
 
     #[test]
     fn test_execute_up_command() {
         let command = "up 2".to_string();
-        let mut position: [u32; 2] = [2, 12];
-        let expected: [u32; 2] = [2, 10];
+        let mut position: Position = Position{ x_pos: 2, y_pos: 12 };
+        let expected: Position = Position{ x_pos: 2, y_pos: 10 };
 
         execute_command(&command, &mut position);
-        assert!(position.iter().zip(expected.iter()).all(|(a,b)| a == b), "Arrays are not equal");
+        assert_eq!(position.x_pos, expected.x_pos, "x_pos are not equal");
+        assert_eq!(position.y_pos, expected.y_pos, "y_pos are not equal");
     }
 
     #[test]
     fn test_execute_down_command() {
         let command = "down 3".to_string();
-        let mut position: [u32; 2] = [2, 12];
-        let expected: [u32; 2] = [2, 15];
+        let mut position: Position = Position{ x_pos: 2, y_pos: 12 };
+        let expected: Position = Position{ x_pos: 2, y_pos: 15};
 
         execute_command(&command, &mut position);
-        assert!(position.iter().zip(expected.iter()).all(|(a,b)| a == b), "Arrays are not equal");
+        assert_eq!(position.x_pos, expected.x_pos, "x_pos are not equal");
+        assert_eq!(position.y_pos, expected.y_pos, "y_pos are not equal");
     }
 
     #[test]
     fn test_drive() {
         let commands = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2\n";
-        let expected: [u32; 2] = [15, 10];
+        let expected: Position = Position{ x_pos: 15, y_pos: 10 };
 
-        let position : [u32; 2] = drive(&mut commands.as_bytes());
-        assert!(position.iter().zip(expected.iter()).all(|(a,b)| a == b), "Arrays are not equal");
+        let position: Position = drive(&mut commands.as_bytes());
+        assert_eq!(position.x_pos, expected.x_pos, "x_pos are not equal");
+        assert_eq!(position.y_pos, expected.y_pos, "y_pos are not equal");
     }
 }
