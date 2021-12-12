c68974ca618c15b6a79424c2de38ca4acafdecb1
Piotr Kubicki <pkubicki44@gmail.com>
2021-12-12 19:45:32 +0000

    Day 3 solution 1

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 struct Report {
     total: u32,
     column_sums: Vec<u32>,
@@ -7,6 +10,7 @@ trait ReportParser {
     fn get_gamma_rate(&self) -> String;
     fn get_epsilon_rate(&self) -> String;
     fn get_power_consumption(&self) -> u32;
+    fn update_report(&mut self, reading: &str);
 }
 
 impl ReportParser for Report {
@@ -15,9 +19,9 @@ impl ReportParser for Report {
 
         for &value in self.column_sums.iter() {
             if value >= (self.total as f32 / 2.0).ceil() as u32 {
-                gamma_rate.push_str(&"1");
+                gamma_rate.push_str("1");
             } else {
-                gamma_rate.push_str(&"0");
+                gamma_rate.push_str("0");
             }
         }
         gamma_rate
@@ -28,9 +32,9 @@ impl ReportParser for Report {
 
         for &value in self.column_sums.iter() {
             if value >= (self.total as f32 / 2.0).ceil() as u32 {
-                epsilon_rate.push_str(&"0");
+                epsilon_rate.push_str("0");
             } else {
-                epsilon_rate.push_str(&"1");
+                epsilon_rate.push_str("1");
             }
         }
         epsilon_rate
@@ -42,9 +46,51 @@ impl ReportParser for Report {
 
         gamma_rate * epsilon_rate
     }
+
+    fn update_report(&mut self, reading: &str) {
+        self.total += 1;
+        if self.column_sums.len() == 0 {
+            self.column_sums = vec![0; reading.len() - 1];
+        }
+
+        for (i, bite) in reading.as_bytes().iter().enumerate() {
+            match bite {
+                b'1' => self.column_sums[i] += 1,
+                _ => continue,
+            }
+        }
+    }
 }
 
-fn main() {}
+fn run<R: BufRead, T: ReportParser>(readings: &mut R, report: &mut T) {
+    let mut first_line = String::new();
+    readings
+        .read_line(&mut first_line)
+        .expect("Cannot read line!");
+    report.update_report(&first_line);
+
+    for reading in readings.lines() {
+        let reading = reading.expect("Cannot read line!");
+        report.update_report(&reading);
+    }
+}
+
+fn task_one() {
+    let file = File::open("src/input.txt").expect("Cannot open file!");
+    let mut readings = BufReader::new(file);
+    let mut report = Report {
+        total: 0,
+        column_sums: vec![],
+    };
+
+    run(&mut readings, &mut report);
+    let power_consumption = report.get_power_consumption();
+    println!("The power consumption is: {}", power_consumption);
+}
+
+fn main() {
+    task_one();
+}
 
 #[cfg(test)]
 mod tests {
@@ -88,5 +134,34 @@ mod tests {
         let actual = report.get_power_consumption();
 
         assert_eq!(actual, expected);
+    }
+
+    #[test_case([12, 4, 0, 2, 11, 0, 9, 8, 8, 0].to_vec(), "0110100111", [12, 5, 1, 2, 12, 0, 9, 9, 9, 1].to_vec())]
+    fn test_update_report(initial_state: Vec<u32>, reading: &str, expected: Vec<u32>) {
+        let mut actual = Report {
+            total: 2,
+            column_sums: initial_state,
+        };
+        actual.update_report(reading);
+
+        assert_eq!(actual.total, 3, "The total value is not equal");
+        for (actual_bit, expected_bit) in actual.column_sums.iter().zip(expected.iter()) {
+            assert_eq!(actual_bit, expected_bit, "The column sum doesn't match");
+        }
+    }
+
+    #[test]
+    fn test_run() {
+        let input =
+            "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010\n";
+        let mut report = Report {
+            total: 0,
+            column_sums: vec![],
+        };
+
+        run(&mut input.as_bytes(), &mut report);
+        let actual = report.get_power_consumption();
+        println!("{} {}", report.get_gamma_rate(), report.get_epsilon_rate());
+        assert_eq!(actual, 198);
     }
 }
