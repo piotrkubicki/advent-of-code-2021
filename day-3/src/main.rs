9ca3c3b8edff76b5f9612a9706a808ed1ed303c0
Piotr Kubicki <pkubicki44@gmail.com>
2021-12-13 23:20:02 +0000

    Filter values
    The find filter functionality still need to be implemented.

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-struct Report {
+struct PowerReport {
     total: u32,
     column_sums: Vec<u32>,
 }
@@ -10,7 +10,7 @@ trait ReportParser {
     fn update_report(&mut self, reading: &str);
 }
 
-impl Report {
+impl PowerReport {
     fn get_gamma_rate(&self) -> String {
         let mut gamma_rate = String::new();
 
@@ -45,7 +45,7 @@ impl Report {
     }
 }
 
-impl ReportParser for Report {
+impl ReportParser for PowerReport {
     fn update_report(&mut self, reading: &str) {
         self.total += 1;
         if self.column_sums.len() == 0 {
@@ -58,6 +58,26 @@ impl ReportParser for Report {
                 _ => continue,
             }
         }
+    }
+}
+
+struct LifeSupportReport {
+    readings: Vec<String>,
+}
+
+impl LifeSupportReport {
+    fn get_oxygen_gen_rate(&mut self) -> String {
+        for i in 0..4 {
+            println!("{:?}", self.readings);
+            let filter: char = self.readings[0].chars().nth(i).unwrap();
+            self.readings = self
+                .readings
+                .iter()
+                .cloned()
+                .filter(|reading| reading.chars().nth(i).unwrap() != filter)
+                .collect();
+        }
+        self.readings[0].clone()
     }
 }
 
@@ -77,7 +97,7 @@ fn run<R: BufRead, T: ReportParser>(readings: &mut R, report: &mut T) {
 fn task_one() {
     let file = File::open("src/input.txt").expect("Cannot open file!");
     let mut readings = BufReader::new(file);
-    let mut report = Report {
+    let mut report = PowerReport {
         total: 0,
         column_sums: vec![],
     };
@@ -100,7 +120,7 @@ mod tests {
     #[test_case(4, [3, 0, 2, 4].to_vec(), r#"1011"#)]
     #[test_case(5, [3, 1, 2, 4, 5].to_vec(), r#"10011"#)]
     fn test_get_gamma_rate_for_report(total: u32, column_sums: Vec<u32>, expected: &str) {
-        let report = Report {
+        let report = PowerReport {
             total: total,
             column_sums: column_sums,
         };
@@ -113,7 +133,7 @@ mod tests {
     #[test_case(4, [3, 0, 2, 4].to_vec(), r#"0100"#)]
     #[test_case(5, [3, 1, 2, 4, 5].to_vec(), r#"01100"#)]
     fn test_get_epsilon_rate_for_report(total: u32, column_sums: Vec<u32>, expected: &str) {
-        let report = Report {
+        let report = PowerReport {
             total: total,
             column_sums: column_sums,
         };
@@ -126,7 +146,7 @@ mod tests {
     #[test_case(4, [3, 0, 2, 4].to_vec(), 11*4)]
     #[test_case(5, [3, 1, 2, 4, 5].to_vec(), 19*12)]
     fn test_get_power_consumption_for_report(total: u32, column_sums: Vec<u32>, expected: u32) {
-        let report = Report {
+        let report = PowerReport {
             total: total,
             column_sums: column_sums,
         };
@@ -137,7 +157,7 @@ mod tests {
 
     #[test_case([12, 4, 0, 2, 11, 0, 9, 8, 8, 0].to_vec(), "0110100111", [12, 5, 1, 2, 12, 0, 9, 9, 9, 1].to_vec())]
     fn test_update_report(initial_state: Vec<u32>, reading: &str, expected: Vec<u32>) {
-        let mut actual = Report {
+        let mut actual = PowerReport {
             total: 2,
             column_sums: initial_state,
         };
@@ -153,7 +173,7 @@ mod tests {
     fn test_run() {
         let input =
             "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010\n";
-        let mut report = Report {
+        let mut report = PowerReport {
             total: 0,
             column_sums: vec![],
         };
@@ -162,5 +182,29 @@ mod tests {
         let actual = report.get_power_consumption();
         println!("{} {}", report.get_gamma_rate(), report.get_epsilon_rate());
         assert_eq!(actual, 198);
+    }
+
+    #[test]
+    fn test_get_oxygen_gen_rate() {
+        let mut report = LifeSupportReport {
+            readings: vec![
+                "00100".to_string(),
+                "11110".to_string(),
+                "10110".to_string(),
+                "10111".to_string(),
+                "10101".to_string(),
+                "01111".to_string(),
+                "00111".to_string(),
+                "11100".to_string(),
+                "10000".to_string(),
+                "11001".to_string(),
+                "00010".to_string(),
+                "01010".to_string(),
+            ],
+        };
+        let expected = "10111".to_string();
+        let actual = report.get_oxygen_gen_rate();
+
+        assert_eq!(actual, expected);
     }
 }
