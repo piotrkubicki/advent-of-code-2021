33ee02b942ecc7ac12bb17aeb559078664d0ffcb
Piotr Kubicki <pkubicki44@gmail.com>
2021-12-14 23:44:53 +0000

    Day 3 task 2 (#1)
    Add day3 solution 1 and 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,19 +1,16 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-struct Report {
+struct PowerReport {
     total: u32,
     column_sums: Vec<u32>,
 }
 
 trait ReportParser {
-    fn get_gamma_rate(&self) -> String;
-    fn get_epsilon_rate(&self) -> String;
-    fn get_power_consumption(&self) -> u32;
     fn update_report(&mut self, reading: &str);
 }
 
-impl ReportParser for Report {
+impl PowerReport {
     fn get_gamma_rate(&self) -> String {
         let mut gamma_rate = String::new();
 
@@ -46,7 +43,9 @@ impl ReportParser for Report {
 
         gamma_rate * epsilon_rate
     }
+}
 
+impl ReportParser for PowerReport {
     fn update_report(&mut self, reading: &str) {
         self.total += 1;
         if self.column_sums.len() == 0 {
@@ -59,6 +58,67 @@ impl ReportParser for Report {
                 _ => continue,
             }
         }
+    }
+}
+
+struct LifeSupportReport {
+    readings: Vec<String>,
+}
+
+impl LifeSupportReport {
+    fn get_oxygen_gen_rate(&mut self) -> u32 {
+        let mut readings: Vec<String> = self.readings.iter().cloned().collect();
+        for i in 0..readings[0].len() {
+            let mut bags = [vec![], vec![]];
+            for reading in &readings {
+                match reading.chars().nth(i).unwrap() {
+                    '1' => bags[1].push(reading.clone()),
+                    '0' => bags[0].push(reading.clone()),
+                    _ => continue,
+                }
+            }
+            readings = bags[(bags[0].len() <= bags[1].len()) as usize]
+                .iter()
+                .cloned()
+                .collect();
+            if readings.len() == 1 {
+                break;
+            }
+        }
+
+        isize::from_str_radix(&readings[0], 2).unwrap() as u32
+    }
+
+    fn get_co2_scrubber_rate(&mut self) -> u32 {
+        let mut readings: Vec<String> = self.readings.iter().cloned().collect();
+        for i in 0..readings[0].len() {
+            let mut bags = [vec![], vec![]];
+            for reading in &readings {
+                match reading.chars().nth(i).unwrap() {
+                    '1' => bags[1].push(reading.clone()),
+                    '0' => bags[0].push(reading.clone()),
+                    _ => continue,
+                }
+            }
+            readings = bags[!(bags[0].len() <= bags[1].len()) as usize]
+                .iter()
+                .cloned()
+                .collect();
+            if readings.len() == 1 {
+                break;
+            };
+        }
+        isize::from_str_radix(&readings[0], 2).unwrap() as u32
+    }
+
+    fn calculate_life_support_rate(&mut self) -> u32 {
+        self.get_oxygen_gen_rate() * self.get_co2_scrubber_rate()
+    }
+}
+
+impl ReportParser for LifeSupportReport {
+    fn update_report(&mut self, reading: &str) {
+        self.readings.push(reading.trim().to_string());
     }
 }
 
@@ -78,7 +138,7 @@ fn run<R: BufRead, T: ReportParser>(readings: &mut R, report: &mut T) {
 fn task_one() {
     let file = File::open("src/input.txt").expect("Cannot open file!");
     let mut readings = BufReader::new(file);
-    let mut report = Report {
+    let mut report = PowerReport {
         total: 0,
         column_sums: vec![],
     };
@@ -88,8 +148,19 @@ fn task_one() {
     println!("The power consumption is: {}", power_consumption);
 }
 
+fn task_two() {
+    let file = File::open("src/input.txt").expect("Cannot open file!");
+    let mut readings = BufReader::new(file);
+    let mut report = LifeSupportReport { readings: vec![] };
+
+    run(&mut readings, &mut report);
+    let life_support_rate = report.calculate_life_support_rate();
+    println!("The Life Support is: {}", life_support_rate);
+}
+
 fn main() {
     task_one();
+    task_two();
 }
 
 #[cfg(test)]
@@ -101,7 +172,7 @@ mod tests {
     #[test_case(4, [3, 0, 2, 4].to_vec(), r#"1011"#)]
     #[test_case(5, [3, 1, 2, 4, 5].to_vec(), r#"10011"#)]
     fn test_get_gamma_rate_for_report(total: u32, column_sums: Vec<u32>, expected: &str) {
-        let report = Report {
+        let report = PowerReport {
             total: total,
             column_sums: column_sums,
         };
@@ -114,7 +185,7 @@ mod tests {
     #[test_case(4, [3, 0, 2, 4].to_vec(), r#"0100"#)]
     #[test_case(5, [3, 1, 2, 4, 5].to_vec(), r#"01100"#)]
     fn test_get_epsilon_rate_for_report(total: u32, column_sums: Vec<u32>, expected: &str) {
-        let report = Report {
+        let report = PowerReport {
             total: total,
             column_sums: column_sums,
         };
@@ -127,7 +198,7 @@ mod tests {
     #[test_case(4, [3, 0, 2, 4].to_vec(), 11*4)]
     #[test_case(5, [3, 1, 2, 4, 5].to_vec(), 19*12)]
     fn test_get_power_consumption_for_report(total: u32, column_sums: Vec<u32>, expected: u32) {
-        let report = Report {
+        let report = PowerReport {
             total: total,
             column_sums: column_sums,
         };
@@ -138,7 +209,7 @@ mod tests {
 
     #[test_case([12, 4, 0, 2, 11, 0, 9, 8, 8, 0].to_vec(), "0110100111", [12, 5, 1, 2, 12, 0, 9, 9, 9, 1].to_vec())]
     fn test_update_report(initial_state: Vec<u32>, reading: &str, expected: Vec<u32>) {
-        let mut actual = Report {
+        let mut actual = PowerReport {
             total: 2,
             column_sums: initial_state,
         };
@@ -154,7 +225,7 @@ mod tests {
     fn test_run() {
         let input =
             "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010\n";
-        let mut report = Report {
+        let mut report = PowerReport {
             total: 0,
             column_sums: vec![],
         };
@@ -163,5 +234,76 @@ mod tests {
         let actual = report.get_power_consumption();
         println!("{} {}", report.get_gamma_rate(), report.get_epsilon_rate());
         assert_eq!(actual, 198);
+    }
+
+    #[test]
+    fn test_get_oxygen_gen_rate() {
+        let mut report = LifeSupportReport {
+            readings: vec![
+                "00100".to_string(),
+                "11110".to_string(),
+                "10110".to_string(),
+                "10111".to_string(),
+                "10101".to_string(),
+                "01111".to_string(),
+                "00111".to_string(),
+                "11100".to_string(),
+                "10000".to_string(),
+                "11001".to_string(),
+                "00010".to_string(),
+                "01010".to_string(),
+            ],
+        };
+        let expected = 23;
+        let actual = report.get_oxygen_gen_rate();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_get_co2_scrubber_rate() {
+        let mut report = LifeSupportReport {
+            readings: vec![
+                "00100".to_string(),
+                "11110".to_string(),
+                "10110".to_string(),
+                "10111".to_string(),
+                "10101".to_string(),
+                "01111".to_string(),
+                "00111".to_string(),
+                "11100".to_string(),
+                "10000".to_string(),
+                "11001".to_string(),
+                "00010".to_string(),
+                "01010".to_string(),
+            ],
+        };
+        let expected = 10;
+        let actual = report.get_co2_scrubber_rate();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_calculate_life_support_rate() {
+        let mut report = LifeSupportReport {
+            readings: vec![
+                "00100".to_string(),
+                "11110".to_string(),
+                "10110".to_string(),
+                "10111".to_string(),
+                "10101".to_string(),
+                "01111".to_string(),
+                "00111".to_string(),
+                "11100".to_string(),
+                "10000".to_string(),
+                "11001".to_string(),
+                "00010".to_string(),
+                "01010".to_string(),
+            ],
+        };
+        let expected = 230;
+        let actual = report.calculate_life_support_rate();
+        assert_eq!(actual, expected);
     }
 }
