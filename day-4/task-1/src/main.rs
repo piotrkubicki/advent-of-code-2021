8f65d73650b7523f470fd1d6328fe5071afe817a
Piotr Kubicki <pkubicki44@gmail.com>
2021-12-18 22:50:23 +0000

    Add Gameboard structure

--- day-4/task-1/src/main.rs
+++ day-4/task-1/src/main.rs
@@ -1,43 +1,55 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn read_data<B: BufRead>(reader: &mut B) -> (Vec<u32>, Vec<[u32; 25]>) {
-    let mut winning_numbers = String::new();
-    let mut gameboards: Vec<[u32; 25]> = vec![];
+struct Gameboard {
+    x: u32,
+    y: u32,
+    values: Vec<u32>,
+}
+
+impl Gameboard {
+    fn build<B: BufRead>(reader: &mut B, x: u32, y: u32) -> Gameboard {
+        let mut values: Vec<u32> = vec![];
+        let total_values: u32 = x * y;
+
+        for line in reader.lines() {
+            let line = line.expect("Corrupted file!");
+            if !line.eq("\n") {
+                let mut parsed_values = line
+                    .split(" ")
+                    .filter(|value| !value.eq(&""))
+                    .map(|value| value.trim().parse::<u32>().unwrap())
+                    .collect();
+                values.append(&mut parsed_values);
+            }
+
+            if values.len() as u32 == total_values {
+                break;
+            }
+        }
+
+        Gameboard {
+            x: x,
+            y: y,
+            values: vec![],
+        }
+    }
+}
+
+fn read_lucky_numbers<B: BufRead>(reader: &mut B) -> Vec<u32> {
+    let mut lucky_numbers = String::new();
 
     reader
-        .read_line(&mut winning_numbers)
+        .read_line(&mut lucky_numbers)
         .expect("Corrupted file");
-    let winning_numbers = winning_numbers
+
+    let lucky_numbers = lucky_numbers
         .split(",")
         .map(|x| x.trim().replace(",", ""))
         .map(|x| x.parse::<u32>().unwrap())
         .collect();
 
-    let mut counter = 0;
-
-    for line in reader.lines() {
-        let line = line.expect("Corrupted file!");
-        println!("the line is {} dd", line);
-        if line.eq("\n") {
-            println!("empty line");
-            counter += 1;
-            gameboards[counter] = [0; 25];
-            println!("dd{:?}", gameboards);
-        } else {
-            for (i, number) in line.split(" ").enumerate() {
-                println!("{}", number);
-                if !number.eq("\n") {
-                    let number = match number.trim().parse() {
-                        Ok(number) => number,
-                        _ => continue,
-                    };
-                    gameboards[counter][i] = number;
-                }
-            }
-        }
-    }
-    (winning_numbers, gameboards)
+    lucky_numbers
 }
 
 fn main() {
@@ -47,30 +59,32 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
+
+    #[test_case("12,8,11,6,7,9", vec![12, 8, 11, 6, 7, 9])]
+    #[test_case("10, 11, 2, 5, 1", vec![10, 11, 2, 5, 1])]
+    fn read_lucky_numbers_return_vector(test_input: &str, expected: Vec<u32>) {
+        let actual = read_lucky_numbers(&mut test_input.as_bytes());
+
+        for (actual, expected) in actual.iter().zip(expected.iter()) {
+            assert_eq!(actual, expected);
+        }
+    }
 
     #[test]
-    fn test_read_data() {
-        let test_input = "12,8,10,11,5,7,9\n\n10 11  9 19  4\n 9 11 4  5 23\n21 11 32 18 12\n22 19 11 12  4\n23 32 12 11  5\n\n10 11  9 19  4\n 8 11 4  5 23\n21 11 32 18 12\n22 19 11 12  4\n23 32 12 11  5";
-        let (actual_numbers, actual_gameboards) = read_data(&mut test_input.as_bytes());
+    fn build_gameboard() {
+        let test_input = "10 11  9 19  4\n 9 11 4  5 23\n21 11 32 18 12\n22 19 11 12  4\n23 32 12 11  5\n\n10 11  9 19  4\n 8 11 4  5 23\n21 11 32 18 12\n22 19 11 12  4\n23 32 12 11  5";
+        let actual = Gameboard::build(&mut test_input.as_bytes(), 5, 5);
         let expected = vec![
-            [
-                10, 11, 9, 19, 4, 9, 11, 4, 5, 23, 21, 11, 32, 18, 12, 22, 19, 11, 12, 4, 23, 32,
-                12, 11, 5,
-            ],
-            [
-                10, 11, 9, 19, 4, 8, 11, 4, 5, 23, 21, 11, 32, 18, 12, 22, 19, 11, 12, 4, 23, 32,
-                12, 11, 5,
-            ],
+            10, 11, 9, 19, 4, 9, 11, 4, 5, 23, 21, 11, 32, 18, 12, 22, 19, 11, 12, 4, 23, 32, 12,
+            11, 5,
         ];
 
-        assert_eq!(
-            actual_numbers,
-            vec![12, 8, 10, 11, 5, 7, 9],
-            "Winning numbers are not as expected!"
-        );
-        assert_eq!(
-            actual_gameboards, expected,
-            "Gameboards are not as expected!"
-        );
+        assert_eq!(actual.y, 5);
+        assert_eq!(actual.x, 5);
+
+        for (actual, expected) in actual.values.iter().zip(expected.iter()) {
+            assert_eq!(actual, expected);
+        }
     }
 }
