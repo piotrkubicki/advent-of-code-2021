9bf2801a066c4e73659b68fd79f251a2ec656b47
Piotr Kubicki <pkubicki44@gmail.com>
2021-12-22 21:50:07 +0000

    Modify gameboard

--- day-4/task-1/src/main.rs
+++ day-4/task-1/src/main.rs
@@ -1,60 +1,84 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Clone, PartialEq, Debug)]
+enum Field {
+    Unmarked(u32),
+    Marked(u32),
+    None,
+}
+
+#[derive(PartialEq, Debug)]
 struct Gameboard {
-    x: u32,
-    y: u32,
-    values: Vec<u32>,
+    id: u32,
+    rows: u32,
+    columns: u32,
+    data: Vec<Vec<Field>>,
 }
 
 impl Gameboard {
-    fn build<B: BufRead>(reader: &mut B, x: u32, y: u32) -> Gameboard {
-        let mut values: Vec<u32> = vec![];
-        let total_values: u32 = x * y;
-
-        for line in reader.lines() {
-            let line = line.expect("Corrupted file!");
-            if !line.eq("\n") {
-                let mut parsed_values = line
-                    .split(" ")
-                    .filter(|value| !value.eq(&""))
-                    .map(|value| value.trim().parse::<u32>().unwrap())
-                    .collect();
-                values.append(&mut parsed_values);
+    fn build(id: u32, rows: u32, columns: u32, numbers: &mut Vec<Vec<u32>>) -> Gameboard {
+        let mut data: Vec<Vec<Field>> = vec![];
+        for values in numbers {
+            let mut row: Vec<Field> = vec![];
+            for &mut value in values {
+                row.push(Field::Unmarked(value));
             }
-
-            if values.len() as u32 == total_values {
-                break;
-            }
+            data.push(row.to_vec());
         }
+        Gameboard{
+            id, rows, columns, data,
+        }
+    }
 
-        Gameboard {
-            x: x,
-            y: y,
-            values: values,
+    fn is_row_all_marked(row: Vec<Field>) -> bool {
+        if row.iter().all(|x| match x {
+            Field::Marked(_) => true,
+            _ => false,
+        }) {
+            return true;
+        } else {
+            false
         }
     }
 }
 
-fn read_lucky_numbers<B: BufRead>(reader: &mut B) -> Vec<u32> {
-    let mut lucky_numbers = String::new();
+fn build_gameboards<T: BufRead>(reader: &mut T, rows: u32, columns: u32) -> Vec<Gameboard> {
+    let mut counter = 0;
+    let mut numbers: Vec<Vec<u32>> = Vec::new();
+    let mut gameboards: Vec<Gameboard> = Vec::new();
 
-    reader
-        .read_line(&mut lucky_numbers)
-        .expect("Corrupted file");
+    for line in reader.lines() {
+        let line = line.expect("Cannot read gameboard data, file may be corrupted!");
+        if !line.eq("\n") && numbers.len() > 0 {
+            gameboards.push(Gameboard::build(counter, rows, columns, &mut numbers));
+            counter += 1;
+            numbers = Vec::new();
+        } else {
+            let clean_numbers = parse_gameboard_data(&line);
+            if clean_numbers.len() > 0 {
+                numbers.push(clean_numbers.to_vec());
+            }
+        }
+    }
+    gameboards
+}
 
-    let lucky_numbers = lucky_numbers
-        .split(",")
-        .map(|x| x.trim().replace(",", ""))
-        .map(|x| x.parse::<u32>().unwrap())
-        .collect();
-
-    lucky_numbers
+fn parse_gameboard_data(data: &str) -> Vec<u32> {
+    data.split(" ")
+        .filter(|x| !x.eq(&""))
+        .map(|x| x.trim().parse::<u32>().unwrap())
+        .collect()
 }
 
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("src/input.txt").expect("File cannot be opened!");
+    let rows = 5;
+    let columns = 5;
+    let mut reader = BufReader::new(file);
+    let mut lucky_numbers = String::new();
+    reader.read_line(&mut lucky_numbers).expect("Cannot read lucky_numbers, file may be corrupted!");
+    let mut gameboards = build_gameboards(&mut reader, rows, columns);
 }
 
 #[cfg(test)]
@@ -62,29 +86,48 @@ mod tests {
     use super::*;
     use test_case::test_case;
 
-    #[test_case("12,8,11,6,7,9", vec![12, 8, 11, 6, 7, 9])]
-    #[test_case("10, 11, 2, 5, 1", vec![10, 11, 2, 5, 1])]
-    fn read_lucky_numbers_return_vector(test_input: &str, expected: Vec<u32>) {
-        let actual = read_lucky_numbers(&mut test_input.as_bytes());
-
-        for (actual, expected) in actual.iter().zip(expected.iter()) {
-            assert_eq!(actual, expected);
-        }
-    }
-
     #[test]
-    fn build_gameboard() {
-        let test_input = "10 11  9 19  4\n 9 11 4  5 23\n21 11 32 18 12\n22 19 11 12  4\n23 32 12 11  5\n\n10 11  9 19  4\n 8 11 4  5 23\n21 11 32 18 12\n22 19 11 12  4\n23 32 12 11  5";
-        let actual = Gameboard::build(&mut test_input.as_bytes(), 5, 5);
+    fn gameboard_build_returns_valid_gameboard() {
+        let columns = 2;
+        let rows = 2;
+        let id = 1;
+        let mut data = vec![vec![1, 2], vec![3, 4]];
         let expected = Gameboard {
-            x: 5,
-            y: 5,
-            values: vec![
-                10, 11, 9, 19, 4, 9, 11, 4, 5, 23, 21, 11, 32, 18, 12, 22, 19, 11, 12, 4, 23, 32,
-                12, 11, 5,
-            ],
+            id,
+            columns,
+            rows,
+            data: vec![
+                vec![Field::Unmarked(1), Field::Unmarked(2)],
+                vec![Field::Unmarked(3), Field::Unmarked(4)],
+            ]
         };
+
+        let actual = Gameboard::build(id, rows, columns, &mut data);
 
         assert_eq!(actual, expected);
     }
+
+    #[test_case("10 11 12 16 18 17" => vec![10, 11, 12, 16, 18, 17])]
+    #[test_case(" 9  7 17  6" => vec![9, 7, 17, 6])]
+    #[test_case(" 2 11  8\n\n" => vec![2, 11, 8])]
+    fn parse_gameboard_data_returns_vector_of_int(input: &str) -> Vec<u32> {
+        parse_gameboard_data(input)
+    }
+
+    #[test]
+    fn build_gameboards_returns_vector_of_gameboards() {
+        let mut input = "10  1 12 17\n 9  7 16 19\n\n12 16 15 14\n11 13  8 15\n\n".as_bytes();
+        let actual = build_gameboards(&mut input, 2, 2);
+        println!("{:?}", actual);
+
+        assert_eq!(actual.len(), 2);
+    }
+
+    #[test_case([Field::Marked(10), Field::Marked(1), Field::Marked(2)].to_vec() => true)]
+    #[test_case([Field::Marked(10), Field::Unmarked(1), Field::Marked(2)].to_vec() => false)]
+    #[test_case([Field::Unmarked(1), Field::Unmarked(11), Field::Unmarked(2)].to_vec() => false)]
+    fn is_row_all_marked_returns_expected(input: Vec<Field>) -> bool {
+        Gameboard::is_row_all_marked(input)
+    }
+
 }
